cfa1c15c54992e307aad88177d7da8c6231b2db5
Vladimir Yermokhin <thankyousomuchimcrazy@gmail.com>
2023-12-07 16:33:47 +0300

    Example002 Hello User

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.InputEncoding = System.Text.Encoding.GetEncoding("utf-16");
+Console.Write("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine($"Привет, {username}");
+
+
