0e6484db3db4a301f83b1889a5a53901be695cdc
Vladimir Yermokhin <thankyousomuchimcrazy@gmail.com>
2023-12-31 10:47:49 +0600

    Example014_RecursionAlgorithm2

---
+++ Example014_RecursionAlgorithm2/Program.cs
@@ -0,0 +1,42 @@
+﻿/* 
+void OpenMatryoshka(int size)
+{
+    if (size == 1)
+    {
+        System.Console.WriteLine("Smallest!");
+        return;
+    }
+    OpenMatryoshka(size - 1);
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+
+}
+
+OpenMatryoshka(5); 
+*/
+
+/* 
+void ShowMsg(string msg)
+{
+    Console.Write(msg + " " );
+}
+ShowMsg("Hello");
+Console.Write("World"); 
+*/
+/* 
+Раскручивание рекурсии - процесс возврата к местам вызова функций
+В памяти сохраняются(стек - область памяти где сохраняются значения):
+Места вызова функции
+Значение параметров функции при вызове 
+*/
+
+int Factorial(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop:{n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(5));
